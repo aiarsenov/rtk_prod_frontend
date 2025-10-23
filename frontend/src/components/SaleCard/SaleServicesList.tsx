36b727330d599a57c69271f217abd68969586201
Владислав Ухлин <heavybird@MacBook-Pro-Vladislav.local>
2025-10-23 13:48:17 +0300

    Карточка продаж. Обновление дизайна

--- frontend/src/components/SaleCard/SaleServicesList.tsx
+++ frontend/src/components/SaleCard/SaleServicesList.tsx
@@ -32,13 +32,26 @@ const SaleServicesList = ({ services, mode, deleteService }) => {
                                 <div className="w-[20px] h-[20px]"></div>
                             ) : (
                                 <button
-                                    className="delete-icon"
+                                    className="delete-button"
                                     title="Удалить услугу"
                                     onClick={(e) => {
                                         e.stopPropagation();
                                         deleteService(item.id);
                                     }}
-                                ></button>
+                                >
+                                    <svg
+                                        width="20"
+                                        height="21"
+                                        viewBox="0 0 20 21"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M5.833 8v9.166h8.333V8h1.667v10c0 .46-.373.833-.833.833H5A.833.833 0 014.166 18V8h1.667zm3.333 0v7.5H7.5V8h1.666zM12.5 8v7.5h-1.667V8H12.5zm0-5.833c.358 0 .677.229.79.57l.643 1.929h2.733v1.667H3.333V4.666h2.733l.643-1.93a.833.833 0 01.79-.57h5zm-.601 1.666H8.1l-.278.833h4.354l-.277-.833z"
+                                            fill="currentColor"
+                                        ></path>
+                                    </svg>
+                                </button>
                             )}
                         </div>
                     </li>
