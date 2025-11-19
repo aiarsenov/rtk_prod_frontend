61b786007d21bb7a6ebd231517a7f9396f9a9f16
summertime-fms <danyushin0107@gmail.com>
2025-11-19 14:12:17 +0300

    Добавлено управление доступом в компоненты Finance, Projects, Sales и Staff с использованием компонента AccessDenied

--- frontend/src/components/Dashboards/Projects/Projects.tsx
+++ frontend/src/components/Dashboards/Projects/Projects.tsx
@@ -1,4 +1,14 @@
+import { useSelector } from "react-redux";
+import { canAccess } from "../../../utils/permissions";
+import AccessDenied from "../../AccessDenied/AccessDenied";
+
 const Projects = () => {
+    const user = useSelector((state: any) => state.user.data);
+
+    if (!canAccess(user, "project_reports")) {
+        return <AccessDenied message="У вас нет прав для просмотра дашборда проектов" />;
+    }
+
     return (
         <div className="container">
             <h1 className="title">В разработке</h1>
