b7e12629050e8d27438c560daf1c52c0ebf0b0ae
Владислав Ухлин <heavybird@MacBook-Pro-Vladislav.local>
2025-12-02 09:37:11 +0300

    Карточка заказчика. Блок Проекты. Доработка элементов списка

--- frontend/src/components/SupplierCard/SupplierCard.tsx
+++ frontend/src/components/SupplierCard/SupplierCard.tsx
@@ -545,7 +545,6 @@ const SupplierCard = () => {
                                         setActiveProject={setActiveProject}
                                         activeProject={activeProject}
                                         getProjectReports={getProjectReports}
-                                        withLink={true}
                                     />
                                 </div>
                             </section>
